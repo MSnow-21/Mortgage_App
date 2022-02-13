a8cd870ee115ed06c704854425ffb06d836b9e3d
Jordan Fly <73404951+MSnow-21@users.noreply.github.com>
2022-02-13 07:38:06 -0500

    Initial component commit to mortgage application

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './Components/Header';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <Header />
+    <div className="Container">
+      <h1>Let's calculate your Mortgage!!!</h1>
+      
+    </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header (){
+    return(
+        <header>
+            <div className="container">
+                <h1>The Mortgage Application!</h1>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
